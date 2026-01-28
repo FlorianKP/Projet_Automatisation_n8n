f5ff733a7c4c48431572b21b91d93660060e015d
Ychnightder <pierreychnightder561@gmail.com>
2026-01-28 10:05:05 +0100

    commande

--- build/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mcp_js_1 = require("@modelcontextprotocol/sdk/server/mcp.js");
-// Create server instance
-const server = new mcp_js_1.McpServer({
-    name: 'n8n',
-    version: '1.0.0',
-});
