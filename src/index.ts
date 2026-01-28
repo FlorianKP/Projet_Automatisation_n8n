80e6f252868d72dc1e11c744f68321fdddb51f00
Ychnightder <pierreychnightder561@gmail.com>
2026-01-28 09:38:31 +0100

    depandance

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
+import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js';
+import { z } from 'zod';
+
+
+// Create server instance
+const server = new McpServer({
+	name: 'n8n',
+	version: '1.0.0',
+});
+    
