2bc239b73d7e76ebd774289754046bd6a30eaa31
Ychnightder <pierreychnightder561@gmail.com>
2026-02-04 09:48:56 +0100

    new db

--- src/seed/mcd.sql
+++ src/seed/mcd.sql
@@ -1,15 +1,21 @@
--- Active: 1770190098723@@127.0.0.1@3306
+-- Active: 1770194470499@@127.0.0.1@3306
 CREATE TABLE ingredient (
     id INTEGER PRIMARY KEY,
     name TEXT NOT NULL,
     type TEXT NOT NULL,
+    quantity INTEGER NOT NULL DEFAULT 0,
     contenant_id INTEGER,
-    FOREIGN KEY (contenant_id) REFERENCES contenants(id)
-    quantity INTEGER NOT NULL DEFAULT 0
-    
-)
+    FOREIGN KEY (contenant_id) REFERENCES Contenant(id)
+);
 
 CREATE TABLE Contenant (
     id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-) 
+    name TEXT NOT NULL
+) ;
+
+INSERT INTO Contenant (name) VALUES ('Frigo');
+INSERT INTO Contenant (name) VALUES ('Placard');
+
+
+INSERT INTO ingredient (name, type, quantity, contenant_id) VALUES ('Tomate', 'Legume', 5, 1);
+INSERT INTO ingredient (name, type, quantity, contenant_id) VALUES ('Pates', 'Feculent', 2, 2);
